1ca2b380ae2fcbad08942364ae5995781142aa4f
kiran mahajan <123790577+Kiran281@users.noreply.github.com>
2025-10-02 15:42:49 +0530

    Add files via upload

---
+++ Constructor_codes/Student.java
@@ -0,0 +1,27 @@
+class Student{
+String name;
+String className;
+static int tuitionFee;
+
+Student(String name, String className){
+this.name = name;
+this.className = className;
+}
+// static block - alway execute first 
+static{
+tuitionFee = 30000;
+}
+
+public  void display(){
+System.out.println("Name:" +name + ",class:" +className  +",Tuition fee:" +tuitionFee);
+}
+  public static void main(String[] args) {
+    Student s1 = new Student("Kiran Mahajan", "BE Computer");
+    Student s2 = new Student("Komal", "BSc IT");
+	
+	s1.display();
+    System.out.println("--------------");
+    s2.display();
+
+}
+}
